269bcf559181c47f7d128018544f1a91cf91177d
Julian Cheal <julian.cheal@gmail.com>
2015-05-02 17:22:14 +0100

    WIP Now prints out iBeacon data

--- spec/lib/adaptor.spec.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var Beacon = source("adaptor");
-
-describe("Cylon.Adaptors.Beacon", function() {
-  var adaptor = new Beacon();
-
-  it("needs tests");
-});
--- spec/lib/cylon-beacon.spec.js
+++ spec/lib/cylon-beacon.spec.js
@@ -2,15 +2,9 @@
 
 var module = source("cylon-beacon");
 
-var Adaptor = source('adaptor'),
-    Driver = source('driver');
+var Driver = source('driver');
 
 describe("Cylon.Beacon", function() {
-  describe("#adaptors", function() {
-    it('is an array of supplied adaptors', function() {
-      expect(module.adaptors).to.be.eql([]);
-    });
-  });
 
   describe("#drivers", function() {
     it('is an array of supplied drivers', function() {
@@ -18,21 +12,15 @@ describe("Cylon.Beacon", function() {
     });
   });
 
-  describe("#dependencies", function() {
-    it('is an array of supplied dependencies', function() {
-      expect(module.dependencies).to.be.eql([]);
-    });
-  });
+  // describe("#dependencies", function() {
+  //   it('is an array of supplied dependencies', function() {
+  //     expect(module.dependencies).to.be.eql([]);
+  //   });
+  // });
 
   describe("#driver", function() {
     it("returns an instance of the Driver", function() {
       expect(module.driver()).to.be.instanceOf(Driver);
-    });
-  });
-
-  describe("#adaptor", function() {
-    it("returns an instance of the Adaptor", function() {
-      expect(module.adaptor()).to.be.instanceOf(Adaptor);
     });
   });
 });
