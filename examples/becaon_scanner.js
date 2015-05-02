d2b648b8467511d04e09f795dbe6f6a4e4b6f71a
Julian Cheal <julian.cheal@gmail.com>
2015-05-02 17:38:24 +0100

    WIP Yay it's working sort of!

--- examples/becaon_scanner.js
+++ examples/becaon_scanner.js
@@ -7,8 +7,20 @@ Cylon.robot({
     beacon: { adaptor: "beacon", module: "cylon-beacon" }
   },
 
+  devices: {
+    ibeacon: { driver: "ble-ibeacon" }
+  },
+
   work: function(my) {
-    // my.beacon.startScanning();
-    console.log(my.beacon)
+    my.beacon.startScanning();
+
+    my.ibeacon.getAdvertisement(function(err, data) {
+      if (!!err) {
+        console.log("error: ", err);
+        return;
+      }
+
+      console.log("data: ", data);
+    });
   }
 }).start();
